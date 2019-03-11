da8218b2a0269bbe7a584332c39be46c374b3518
nicolas-trouin <nicolas.trouin@etu.univ-amu.fr>
2019-03-11 22:48:57 +0100

    Toast qui affiche le nom du bateau au click

--- app/src/main/java/com/example/boattracker/MainActivity.java
+++ app/src/main/java/com/example/boattracker/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.boattracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<ContainerShip> bateaux = new ArrayList<>();
+        final List<ContainerShip> bateaux = new ArrayList<>();
         bateaux.add(new ContainerShip("Le Poséïdon","Marcel"));
         bateaux.add(new ContainerShip("Le Zeus","Fred"));
         bateaux.add(new ContainerShip("Le Hadès","Jacques"));
@@ -23,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
         ListView shipListView = findViewById(R.id.ship_list_view);
         shipListView.setAdapter(new ContainerShipAdapter(this, bateaux));
+
+        shipListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), bateaux.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
