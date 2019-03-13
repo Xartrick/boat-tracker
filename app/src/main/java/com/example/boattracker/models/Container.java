a45dba3f60fa468c32bec484424c0361cf8529a7
nicolas-trouin <nicolas.trouin@etu.univ-amu.fr>
2019-03-13 14:01:48 +0100

    Added class Containers

---
+++ app/src/main/java/com/example/boattracker/models/Container.java
@@ -0,0 +1,15 @@
+package com.example.boattracker.models;
+
+public class Container {
+    private String id;
+    private int length;
+    private int heigth;
+    private int width;
+
+    public Container(String id, int length, int heigth, int width) {
+        this.id = id;
+        this.length = length;
+        this.heigth = heigth;
+        this.width = width;
+    }
+}
