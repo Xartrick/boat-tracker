6cbb6543fae7e1e3a3f63f743d20ef37d8b951e7
nicolas-trouin <nicolas.trouin@etu.univ-amu.fr>
2019-03-13 14:04:07 +0100

    Edited class Containership

--- app/src/main/java/com/example/boattracker/models/Containership.java
+++ app/src/main/java/com/example/boattracker/models/Containership.java
@@ -1,12 +1,33 @@
 package com.example.boattracker.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Containership {
+    private String id;
     private String name;
     private String captainName;
+    private double latitude;
+    private double longitude;
+    private ContainershipType type;
+    private List<Container> containers;
 
-    public Containership(String name, String captainName) {
+    public Containership(String id, String name, String captainName, double latitude, double longitude, ContainershipType type) {
+        this.id = id;
         this.name = name;
         this.captainName = captainName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.type = type;
+        this.containers = new ArrayList<>();
+    }
+
+    public void addContainer(Container container){
+        this.containers.add(container);
+    }
+
+    public void removeContainer(Container container){
+        this.containers.remove(container);
     }
 
     public String getName() {
