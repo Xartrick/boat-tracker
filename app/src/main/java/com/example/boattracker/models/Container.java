38a050cb26bb3e922bf19a5638a8a790214c44a6
nicolas-trouin <nicolas.trouin@etu.univ-amu.fr>
2019-03-13 14:12:38 +0100

    Added getter and setter

--- app/src/main/java/com/example/boattracker/models/Container.java
+++ app/src/main/java/com/example/boattracker/models/Container.java
@@ -12,4 +12,36 @@ public class Container {
         this.heigth = heigth;
         this.width = width;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getHeigth() {
+        return heigth;
+    }
+
+    public void setHeigth(int heigth) {
+        this.heigth = heigth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
 }
