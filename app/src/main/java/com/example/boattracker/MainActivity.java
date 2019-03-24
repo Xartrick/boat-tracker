c42a76419c25630591cafa658cfea04f3b302724
Xartrick <xartrick@gmail.com>
2019-03-24 20:28:18 +0100

    Feature containers (#26)
    * Change HasLocalization to HasPosition
    
    * Add comments
    
    * Add containers methods
    
    * Edit main activity

--- app/src/main/java/com/example/boattracker/MainActivity.java
+++ app/src/main/java/com/example/boattracker/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import com.example.boattracker.adapters.ContainershipAdapter;
+import com.example.boattracker.models.Container;
 import com.example.boattracker.models.Containership;
 import com.example.boattracker.models.ContainershipType;
 import com.example.boattracker.models.Port;
@@ -27,15 +28,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getData() {
-        ContainershipType type = new ContainershipType("1", "Type", 10, 10, 10);
-        Port port = new Port("1", "Marseille", 43.296482, 5.369780); // Marseille
+        final ContainershipType type = new ContainershipType("1", "Type", 10, 10, 10);
+
+        final Port port = new Port("1", "Marseille", 43.296482, 5.369780); // Marseille
+
+        final Container container1 = new Container("1", 1, 1, 1);
+        final Container container2 = new Container("2", 2, 2, 2);
+        final Container container3 = new Container("3", 3, 3, 3);
+
+        final Containership containership1 = new Containership("1", "Le Poséïdon", "Marcel", 43.524910, 5.454140, port, type); // Aix-en-Provence
+        final Containership containership2 = new Containership("2", "Le Zeus", "Fred", 0, 0, port, type);
+        final Containership containership3 = new Containership("3", "Le Hadès", "Jacques", 0, 0, port, type);
+        final Containership containership4 = new Containership("4", "L'Apollon", "Brigitte", 0, 0, port, type);
+
+        containership1.addContainer(container1);
+        containership1.addContainer(container2);
+
+        containership1.addContainer(container3);
 
         this.containerships = new ArrayList<>();
 
-        this.containerships.add(new Containership("1", "Le Poséïdon", "Marcel", 43.524910, 5.454140, port, type)); // Aix-en-Provence
-        this.containerships.add(new Containership("2", "Le Zeus", "Fred", 0, 0, port, type));
-        this.containerships.add(new Containership("3", "Le Hadès", "Jacques", 0, 0, port, type));
-        this.containerships.add(new Containership("4", "L'Apollon", "Brigitte", 0, 0, port, type));
+        this.containerships.add(containership1);
+        this.containerships.add(containership2);
+        this.containerships.add(containership3);
+        this.containerships.add(containership4);
     }
 
     private void drawUI() {
--- app/src/main/java/com/example/boattracker/models/Containership.java
+++ app/src/main/java/com/example/boattracker/models/Containership.java
@@ -2,13 +2,15 @@ package com.example.boattracker.models;
 
 import com.example.boattracker.documents.BaseDocument;
 import com.example.boattracker.models.traits.HasId;
-import com.example.boattracker.models.traits.HasLocalization;
+import com.example.boattracker.models.traits.HasPosition;
 import com.example.boattracker.models.traits.HasName;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Containership extends BaseDocument implements HasId, HasName, HasLocalization {
+public class Containership
+        extends BaseDocument
+        implements HasId, HasName, HasPosition {
 
     private String captainName;
     private Port port;
@@ -21,7 +23,7 @@ public class Containership extends BaseDocument implements HasId, HasName, HasLo
 
         this.captainName = captainName;
 
-        this.setLocalization(latitude, longitude);
+        this.setPosition(latitude, longitude);
 
         this.port = port;
         this.type = type;
@@ -53,10 +55,109 @@ public class Containership extends BaseDocument implements HasId, HasName, HasLo
     }
 
     public List<Container> getContainers() {
-        return containers;
+        return this.containers;
     }
 
     public void setContainers(List<Container> containers) {
         this.containers = containers;
+    }
+
+    /**
+     * Add a Container
+     *
+     * @param container Container to add
+     * @return True if Container has been added, false otherwise
+     */
+    public boolean addContainer(Container container) {
+        if (this.hasContainer(container)) {
+            return false;
+        }
+
+        if (!this.canContainContainer(container)) {
+            return false;
+        }
+
+        this.containers.add(container);
+
+        return true;
+    }
+
+    /**
+     * Move a Container from a Containership
+     *
+     * @param containership Source Containership
+     * @param container Container to move
+     * @return True if Container has been moved, false otherwise
+     */
+    public boolean moveContainerFromContainership(Containership containership, Container container) {
+        if (!containership.hasContainer(container)) {
+            return false;
+        }
+
+        if (!this.isContainershipCloseEnough(containership)) {
+            return false;
+        }
+
+        if (!this.addContainer(container)) {
+            return false;
+        }
+
+        return containership.removeContainer(container);
+    }
+
+    /**
+     * Remove a Container
+     *
+     * @param container Container
+     * @return True if Container has been removed, false otherwise
+     */
+    public boolean removeContainer(Container container) {
+        if (!this.hasContainer(container)) {
+            return false;
+        }
+
+        this.containers.remove(container);
+
+        return true;
+    }
+
+    /**
+     * Check if Container is in Container list
+     * @param container Container
+     * @return True if Container is in Container list, false otherwise
+     */
+    public boolean hasContainer(Container container) {
+        final List<Container> containers = this.getContainers();
+
+        return containers.contains(container);
+    }
+
+    /**
+     * Get used volume by containers
+     *
+     * @return Containers used volume
+     */
+    public int getContainersVolume() {
+        int volume = 0;
+
+        final List<Container> containers = this.getContainers();
+
+        for (Container container : containers) {
+            volume += container.getVolume();
+        }
+
+        return volume;
+    }
+
+    public boolean canContainContainer(Container container) {
+        int volume = container.getVolume();
+        int max_volume = this.getType().getVolume();
+        int used_volume = this.getContainersVolume();
+
+        return (used_volume + volume <= max_volume);
+    }
+
+    public boolean isContainershipCloseEnough(Containership containership) {
+        return this.getDistance(containership) <= 300;
     }
 }
