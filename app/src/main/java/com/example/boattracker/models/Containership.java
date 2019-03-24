ad1a654701d9e49aa43fd2cd4de180fc427cfafb
Xartrick <xartrick@gmail.com>
2019-03-24 20:21:42 +0100

    Add containers methods

--- app/src/main/java/com/example/boattracker/models/Containership.java
+++ app/src/main/java/com/example/boattracker/models/Containership.java
@@ -55,10 +55,109 @@ public class Containership
     }
 
     public List<Container> getContainers() {
-        return containers;
+        return this.containers;
     }
 
     public void setContainers(List<Container> containers) {
         this.containers = containers;
+    }
+
+    /**
+     * Add a Container
+     *
+     * @param container Container to add
+     * @return True if Container has been added, false otherwise
+     */
+    public boolean addContainer(Container container) {
+        if (this.hasContainer(container)) {
+            return false;
+        }
+
+        if (!this.canContainContainer(container)) {
+            return false;
+        }
+
+        this.containers.add(container);
+
+        return true;
+    }
+
+    /**
+     * Move a Container from a Containership
+     *
+     * @param containership Source Containership
+     * @param container Container to move
+     * @return True if Container has been moved, false otherwise
+     */
+    public boolean moveContainerFromContainership(Containership containership, Container container) {
+        if (!containership.hasContainer(container)) {
+            return false;
+        }
+
+        if (!this.isContainershipCloseEnough(containership)) {
+            return false;
+        }
+
+        if (!this.addContainer(container)) {
+            return false;
+        }
+
+        return containership.removeContainer(container);
+    }
+
+    /**
+     * Remove a Container
+     *
+     * @param container Container
+     * @return True if Container has been removed, false otherwise
+     */
+    public boolean removeContainer(Container container) {
+        if (!this.hasContainer(container)) {
+            return false;
+        }
+
+        this.containers.remove(container);
+
+        return true;
+    }
+
+    /**
+     * Check if Container is in Container list
+     * @param container Container
+     * @return True if Container is in Container list, false otherwise
+     */
+    public boolean hasContainer(Container container) {
+        final List<Container> containers = this.getContainers();
+
+        return containers.contains(container);
+    }
+
+    /**
+     * Get used volume by containers
+     *
+     * @return Containers used volume
+     */
+    public int getContainersVolume() {
+        int volume = 0;
+
+        final List<Container> containers = this.getContainers();
+
+        for (Container container : containers) {
+            volume += container.getVolume();
+        }
+
+        return volume;
+    }
+
+    public boolean canContainContainer(Container container) {
+        int volume = container.getVolume();
+        int max_volume = this.getType().getVolume();
+        int used_volume = this.getContainersVolume();
+
+        return (used_volume + volume <= max_volume);
+    }
+
+    public boolean isContainershipCloseEnough(Containership containership) {
+        return this.getDistance(containership) <= 300;
     }
 }
