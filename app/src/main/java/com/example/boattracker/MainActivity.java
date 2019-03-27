df191cf213e0e992745310f246ee27e71c0e6b71
Xartrick <xartrick@gmail.com>
2019-03-27 09:43:43 +0100

    Add missing import

--- app/src/main/java/com/example/boattracker/MainActivity.java
+++ app/src/main/java/com/example/boattracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.boattracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import com.example.boattracker.adapters.ContainershipAdapter;
