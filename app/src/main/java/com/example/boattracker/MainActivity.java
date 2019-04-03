1772c139e5970d849c77d4c8c97ad882827c1fda
Xartrick <xartrick@gmail.com>
2019-04-03 11:33:13 +0200

    Remove unused code

--- app/src/main/java/com/example/boattracker/MainActivity.java
+++ app/src/main/java/com/example/boattracker/MainActivity.java
@@ -70,31 +70,6 @@ public class MainActivity extends AppCompatActivity {
                     Log.w("FIRESTORE", "Error getting documents.", task.getException());
                 }
             });
-
-        /* final ContainershipType type = new ContainershipType("1", "Type", 10, 10, 10);
-
-        final Port port = new Port("1", "Marseille", 43.296482, 5.369780); // Marseille
-
-        final Container container1 = new Container("1", 1, 1, 1);
-        final Container container2 = new Container("2", 2, 2, 2);
-        final Container container3 = new Container("3", 3, 3, 3);
-
-        final Containership containership1 = new Containership("1", "Le Poséïdon", "Marcel", 43.524910, 5.454140, port, type); // Aix-en-Provence
-        final Containership containership2 = new Containership("2", "Le Zeus", "Fred", 0, 0, port, type);
-        final Containership containership3 = new Containership("3", "Le Hadès", "Jacques", 0, 0, port, type);
-        final Containership containership4 = new Containership("4", "L'Apollon", "Brigitte", 0, 0, port, type);
-
-        containership1.addContainer(container1);
-        containership1.addContainer(container2);
-
-        containership1.addContainer(container3);
-
-        this.containerships = new ArrayList<>();
-
-        this.containerships.add(containership1);
-        this.containerships.add(containership2);
-        this.containerships.add(containership3);
-        this.containerships.add(containership4); */
     }
 
     private void drawUI() {
