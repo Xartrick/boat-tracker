5d21629339450a1cda8cbb0e092b4b5ad07251a7
Xartrick <xartrick@gmail.com>
2019-04-03 10:44:07 +0200

    Add Position class

--- app/src/main/java/com/example/boattracker/models/Position.java
+++ app/src/main/java/com/example/boattracker/models/Position.java
@@ -1,4 +1,35 @@
 package com.example.boattracker.models;
 
-public class Position {
+import java.io.Serializable;
+
+public class Position implements Serializable {
+
+    private double latitude;
+    private double longitude;
+
+    public Position() {
+        this.setLatitude(0.0);
+        this.setLongitude(0.0);
+    }
+
+    public Position(double latitude, double longitude) {
+        this.setLatitude(latitude);
+        this.setLongitude(longitude);
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
