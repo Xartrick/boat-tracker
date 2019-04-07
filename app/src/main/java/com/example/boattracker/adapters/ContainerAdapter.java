f6e0cb95849c6cec9a9ec927ba8632e734f2f508
Xartrick <xartrick@gmail.com>
2019-04-07 15:44:54 +0200

    Display containership available volume

--- app/src/main/java/com/example/boattracker/adapters/ContainerAdapter.java
+++ app/src/main/java/com/example/boattracker/adapters/ContainerAdapter.java
@@ -47,7 +47,7 @@ public class ContainerAdapter extends BaseAdapter {
         Container container = (Container) getItem(position);
 
         TextView lengthTextView = convertView.findViewById(R.id.container_volume);
-        lengthTextView.setText(Integer.toString(container.getLength()) + "x" + Integer.toString(container.getWidth())  + "x" + Integer.toString(container.getHeight()));
+        lengthTextView.setText(Integer.toString(container.getLength()) + "x" + Integer.toString(container.getWidth())  + "x" + Integer.toString(container.getHeight()) + " (" + container.getVolume() + " m3)");
 
         TextView idTextView = convertView.findViewById(R.id.container_id);
         idTextView.setText("Container " + (position + 1));
